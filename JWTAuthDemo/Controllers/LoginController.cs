d21a2cba201cca38b667f88dc6ce2c6f23fefb27
Moti Elmakies <motiphone2003@gmail.com>
2020-10-23 15:15:20 +0300

    Progress

--- JWTAuthDemo/Controllers/LoginController.cs
+++ JWTAuthDemo/Controllers/LoginController.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using JWTAuthDemo.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JWTAuthDemo.Controllers
 {
@@ -30,7 +36,13 @@ namespace JWTAuthDemo.Controllers
             IActionResult response = Unauthorized();
 
 
-            var user =  AuthenticeteUser(login)
+            var user = AuthenticeteUser(login);
+
+            if(user != null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
 
 
 
@@ -51,6 +63,32 @@ namespace JWTAuthDemo.Controllers
 
             }
             return user;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var Claims = new[]
+            {
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Email, userInfo.EmailAddress),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+
+
+            var token = new JwtSecurityToken(
+                    issuer: _config["Jwt:Issuer"],
+                    audience: _config["Jwt:Issuer"],
+                    Claims,
+                    expires: DateTime.Now.AddMinutes(120),
+                    signingCredentials: credentials
+                );
+
+            var encodedtoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodedtoken;
         }
     }
 }
